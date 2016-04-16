ebca57dd86bb9a3bbc95c1158500d0ba29025648
Victor <victor.sunlixin@gmail.com>
2016-04-16 20:27:19 +0800

    update

--- LaundryButler/LaundryButler-war/src/java/jsf/managedbean/CustomerCartManagedBean.java
+++ LaundryButler/LaundryButler-war/src/java/jsf/managedbean/CustomerCartManagedBean.java
@@ -118,7 +118,7 @@ public class CustomerCartManagedBean implements Serializable {
         accountManagementRemote.removeCartLineItemFromCart(cartLineItemToRemove);
     }
     
-    public Boolean isCartEmpty(){
+    public boolean isCartEmpty(){
         for (int i = 0; i < cartLineItems.size(); i ++){
             if (cartLineItems.get(i).getQuantity() == 0){
                 return false;
