12f3cb9ee90d29ec2dcf6909bf929638c6ce8b2e
Victor <victor.sunlixin@gmail.com>
2016-04-17 17:04:37 +0800

    update webpage

---
+++ LaundryButler/LaundryButler-war/web/resources/js/emp_nav_control.js
@@ -0,0 +1,35 @@
+$(function(){
+          $(".subscreen").hide();
+          $("#home").show();
+          
+          $("#nav_home").click(function(){
+            $(".subscreen").hide();
+            $("#home").show();
+          });
+          
+          $("#nav_mybox").click(function(){
+            $(".subscreen").hide();
+            $("#mybox").show();
+          });
+          
+          $("#footer_up").click(function(){
+            $(".subscreen").hide();
+            $("#home").show();
+          });
+          
+          $("#nav_mybox").click(function(){
+            $(".subscreen").hide();
+            $("#mybox").show();
+          });
+          
+          $("#nav_management").click(function(){
+            $(".subscreen").hide();
+            $("#management").show();
+          });
+          
+          $("#nav_product").click(function(){
+            $(".subscreen").hide();
+            $("#product").show();
+          });
+          
+        });
