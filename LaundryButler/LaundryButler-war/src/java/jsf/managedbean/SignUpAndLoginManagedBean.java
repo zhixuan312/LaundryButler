6310df4379d65bfd04744aa45cfa18ffe5694de5
Ho Ren Sen <horensen@live.com>
2016-04-19 14:14:23 +0800

    Formatted SignUpAndLoginManagedBean.java - no changes

--- LaundryButler/LaundryButler-war/src/java/jsf/managedbean/SignUpAndLoginManagedBean.java
+++ LaundryButler/LaundryButler-war/src/java/jsf/managedbean/SignUpAndLoginManagedBean.java
@@ -16,74 +16,60 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.servlet.http.HttpSession;
 
-
 @Named(value = "signUpAndLoginManagedBean")
 @SessionScoped
 
-public class SignUpAndLoginManagedBean implements Serializable
-{
+public class SignUpAndLoginManagedBean implements Serializable {
+
     @EJB
     private AccountManagementRemote accountManagementRemote;
-    
+
     private Customer customer;
     private String email;
     private String password;
     private String last_name;
     private String first_name;
     private String gender;
-    
+
     @PostConstruct
-    public void init()
-    {
+    public void init() {
         FacesContext.getCurrentInstance().getExternalContext().getSession(true);
     }
-    
-    public SignUpAndLoginManagedBean()
-    {
+
+    public SignUpAndLoginManagedBean() {
         customer = new Customer();
         customer.setIsFaceBook(false);
         email = "";
         password = "";
-        last_name="";
-        first_name="";
-        gender="";
+        last_name = "";
+        first_name = "";
+        gender = "";
     }
-    
-    public void customerLogin(ActionEvent event)
-    {
-        try
-        {
+
+    public void customerLogin(ActionEvent event) {
+        try {
             ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-            
-            if(accountManagementRemote.customerLogin(email, password))
-            {
+
+            if (accountManagementRemote.customerLogin(email, password)) {
                 ec.getSessionMap().put("login", true);
                 ec.redirect("http://localhost:8080/LaundryButler-war/home.xhtml?faces-redirect=true");
-            }
-            else
-            {
+            } else {
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid login credential", "Invalid login credential"));
             }
-            
-        }
-        catch(IOException ex)
-        {
+
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-    
-    public void customerLoginByFaceBook(ActionEvent event){
-        try
-        {
+
+    public void customerLoginByFaceBook(ActionEvent event) {
+        try {
             ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-            
-            if(accountManagementRemote.customerLoginByFaceBook(email))
-            {
+
+            if (accountManagementRemote.customerLoginByFaceBook(email)) {
                 ec.getSessionMap().put("login", true);
                 ec.redirect("http://localhost:8080/LaundryButler-war/home.xhtml?faces-redirect=true");
-            }
-            else
-            {
+            } else {
                 this.customer.setEmail(email);
                 SecureRandom random = new SecureRandom();
                 this.customer.setPassword(new BigInteger(130, random).toString(32));
@@ -93,144 +79,122 @@ public class SignUpAndLoginManagedBean implements Serializable
                 this.customer.setIsFaceBook(true);
                 this.createCustomer(event);
             }
-            
-        }
-        catch(IOException ex)
-        {
+
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
-        
+
     }
-    
-    public void employeeLogin(ActionEvent event)
-    {
-        try
-        {
+
+    public void employeeLogin(ActionEvent event) {
+        try {
             ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-            
-            if(accountManagementRemote.employeeLogin(email, password))
-            {
+
+            if (accountManagementRemote.employeeLogin(email, password)) {
                 ec.getSessionMap().put("login", true);
                 ec.redirect("http://localhost:8080/LaundryButler-war/employee.xhtml?faces-redirect=true");
-            }
-            else
-            {
+            } else {
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid login credential", "Invalid login credential"));
             }
-            
-        }
-        catch(IOException ex)
-        {
+
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-    
-    public void cancelLogin(ActionEvent event)
-    {
-        try
-        {
+
+    public void cancelLogin(ActionEvent event) {
+        try {
             ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-            
+
             ec.redirect("SignUpAndLogin.xhtml?faces-redirect=true");
-            
-        }
-        catch(IOException ex)
-        {
+
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-    
-    public void logout(ActionEvent event)
-    {
-        try
-        {
+
+    public void logout(ActionEvent event) {
+        try {
             accountManagementRemote.logout();
             ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-            
-            ((HttpSession)ec.getSession(true)).invalidate();
-            ec.redirect("index.xhtml?faces-redirect=true");      
-        }
-        catch(IOException ex)
-        {
+
+            ((HttpSession) ec.getSession(true)).invalidate();
+            ec.redirect("index.xhtml?faces-redirect=true");
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-    
-    public void createCustomer(ActionEvent event)
-    {
+
+    public void createCustomer(ActionEvent event) {
         String verificationCode = accountManagementRemote.register(customer);
-        if (!verificationCode.equals("-1")){
+        if (!verificationCode.equals("-1")) {
             this.email = customer.getEmail();
             this.password = customer.getPassword();
             customer = new Customer();
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "New customer registered successfully!", "Your registration verification code is " + verificationCode));
             this.customerLogin(event);
-            
         } else {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Email is been registered", "Email is been registered"));        }
-    }
-    
-    public void cancelRegister(ActionEvent event)
-    {
-        try
-        {
-            ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-            
-            ec.redirect("SignUpAndLogin.xhtml?faces-redirect=true");
-            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Email is been registered", "Email is been registered"));
         }
-        catch(IOException ex)
-        {
+    }
+
+    public void cancelRegister(ActionEvent event) {
+        try {
+            ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+
+            ec.redirect("SignUpAndLogin.xhtml?faces-redirect=true");
+
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-    
+
     public Customer getCustomer() {
         return customer;
     }
-    
+
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
-    
+
     public String getEmail() {
         return email;
     }
-    
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
     public String getPassword() {
         return password;
     }
-    
+
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     public String getLast_name() {
         return last_name;
     }
-    
+
     public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
-    
+
     public String getFirst_name() {
         return first_name;
     }
-    
+
     public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
-    
+
     public String getGender() {
         return gender;
     }
-    
+
     public void setGender(String gender) {
         this.gender = gender;
     }
-    
+
 }
