c8c9c95372dac89e7dcd9a82693978e5b0b92e1a
Ho Ren Sen <horensen@live.com>
2016-05-03 02:13:49 +0800

    Completed customers management

--- LaundryButler/LaundryButler-war/src/java/jsf/managedbean/AdminCustomerManagedBean.java
+++ LaundryButler/LaundryButler-war/src/java/jsf/managedbean/AdminCustomerManagedBean.java
@@ -3,6 +3,7 @@ package jsf.managedbean;
 import AccountManagement.AccountManagementRemote;
 import entity.Customer;
 import entity.Employee;
+import entity.Product;
 import java.io.IOException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
@@ -15,6 +16,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import org.primefaces.event.CellEditEvent;
 
 @Named(value = "adminCustomerManagedBean")
 @SessionScoped
@@ -58,6 +60,20 @@ public class AdminCustomerManagedBean implements Serializable {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success!", "Success!"));
         } else {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Fail to update", "Fail to update"));
+        }
+    }
+    
+    public void onCellEdit(CellEditEvent event) {
+        Object oldValue = event.getOldValue();
+        Object newValue = event.getNewValue();
+         
+        if(newValue != null && !newValue.equals(oldValue)) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            selectCustomer = context.getApplication().evaluateExpressionGet(context, "#{customer}", Customer.class);
+            accountManagementRemote.updateCutomerProfile(selectCustomer);
+            
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
         }
     }
 
