edadd0ba00f6d54daa2d810f20cf9e5fdca2b4ef
victor <victor.sunlixin@gmail.com>
2016-05-03 22:29:34 +0800

    update customer box page

--- LaundryButler/LaundryButler-war/src/java/jsf/managedbean/CheckOutManagedBean.java
+++ LaundryButler/LaundryButler-war/src/java/jsf/managedbean/CheckOutManagedBean.java
@@ -17,6 +17,7 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import com.stripe.Stripe;
 import com.stripe.model.Charge;
+import entity.Address;
 import entity.Box;
 import entity.CartLineItem;
 import entity.SharedBoxPermission;
@@ -164,6 +165,7 @@ public class CheckOutManagedBean implements Serializable {
                                     box.setDryCleaning(0);
                                     box.setPayer(customer);
                                     box.setIsExpress(false);
+                                    //box.setAddress(new Address());
                                     box.setStatus("Unscheduled");
                                     box.setSharedBoxPermissions(new ArrayList<SharedBoxPermission>());
                                     double price = 0;
