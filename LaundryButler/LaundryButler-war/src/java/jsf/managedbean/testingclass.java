a8ae9eb9b9bd989f0a25ff00321b4257063c87e8
Ho Ren Sen <horensen@live.com>
2016-05-03 20:47:13 +0800

    transferring sample code

---
+++ LaundryButler/LaundryButler-war/src/java/jsf/managedbean/testingclass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.managedbean;
+
+import java.util.Map;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author HoRenSen
+ */
+public class testingclass {
+    
+    //Map<String, String> parameters = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+      //  auctionListingId = Long.parseLong(parameters.get("id"));
+    
+}
