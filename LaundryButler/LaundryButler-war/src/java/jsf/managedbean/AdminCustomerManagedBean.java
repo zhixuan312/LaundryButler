08f03320368c3196b58966a467b3f2672e1e6231
windynguyen93 <windynguyen93@gmail.com>
2016-05-04 16:30:47 +0800

    Send Announcement to customers

--- LaundryButler/LaundryButler-war/src/java/jsf/managedbean/AdminCustomerManagedBean.java
+++ LaundryButler/LaundryButler-war/src/java/jsf/managedbean/AdminCustomerManagedBean.java
@@ -1,15 +1,19 @@
 package jsf.managedbean;
 
 import AccountManagement.AccountManagementRemote;
+import com.twilio.sdk.TwilioRestException;
 import entity.Customer;
 import entity.Employee;
 import entity.Product;
+import extensions.TextSender;
 import java.io.IOException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
@@ -28,6 +32,7 @@ public class AdminCustomerManagedBean implements Serializable {
     private Employee admin;
     private List<Customer> customers;
     private Customer selectCustomer;
+    private String textMessage;
 
     public AdminCustomerManagedBean() {
         admin = new Employee();
@@ -76,6 +81,21 @@ public class AdminCustomerManagedBean implements Serializable {
             FacesContext.getCurrentInstance().addMessage(null, msg);
         }
     }
+    
+    public void sendTextAnnouncement(ActionEvent event) {
+        
+        TextSender ts = new TextSender();       
+        ts.setBodyMessage(textMessage);
+        
+        for (Customer customer:customers) {
+            ts.setRecipientPhoneNumber(customer.getMobile());
+            try {
+                ts.sendText();
+            } catch (TwilioRestException ex) {
+                // phone no. is not valid
+            }
+        }
+    }
 
     public Employee getAdmin() {
         return admin;
@@ -101,4 +121,20 @@ public class AdminCustomerManagedBean implements Serializable {
         this.selectCustomer = selectCustomer;
     }
 
+    public String getTextMessage() {
+        return textMessage;
+    }
+
+    public void setTextMessage(String textMessage) {
+        this.textMessage = textMessage;
+    }
+
+    public AccountManagementRemote getAccountManagementRemote() {
+        return accountManagementRemote;
+    }
+
+    public void setAccountManagementRemote(AccountManagementRemote accountManagementRemote) {
+        this.accountManagementRemote = accountManagementRemote;
+    }
+    
 }
